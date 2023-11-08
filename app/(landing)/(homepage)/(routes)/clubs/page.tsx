291edcc5a66b9100af0ae65bb3828e72ff660315
Ernest Nnamdi <Bulaveur@ErnestMackbookAir.local>
2023-11-08 15:21:36 +0100

    added club features. save images

--- app/(landing)/(homepage)/(routes)/clubs/page.tsx
+++ app/(landing)/(homepage)/(routes)/clubs/page.tsx
@@ -1,4 +1,5 @@
 import ClubFeatures from "@/components/landingPage/clubs/ClubFeatures";
+import Faq from "@/components/landingPage/clubs/Faq";
 import Header from "@/components/landingPage/clubs/Header";
 import HowItWorks from "@/components/landingPage/clubs/HowItWorks";
 import React from "react";
@@ -9,6 +10,7 @@ const Clubs = () => {
       <Header />
       <HowItWorks />
       <ClubFeatures />
+      <Faq />
     </div>
   );
 };
